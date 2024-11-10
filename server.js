45714d03e5172194d6c3597f46fd925289457f2d
YuriyKap <yuriy.kk.04@gmail.com>
2024-11-10 13:42:22 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
+// Middleware to parse JSON data from POST requests
+app.use(bodyParser.json());
+
 // Serve the HTML file on a GET request
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Endpoint to receive GPS data from client
+app.post('/location', (req, res) => {
+    const { latitude, longitude } = req.body;
+    console.log(`Received GPS Data - Latitude: ${latitude}, Longitude: ${longitude}`);
+    res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
